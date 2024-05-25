a38ab7fbcddac9b07682efbccff866c0c1ef83bd
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2024-05-24 23:08:30 -0400

    chore: Add gitignore file and update build system for assets and libraries

---
+++ src/config.hpp
@@ -0,0 +1,58 @@
+// config.hpp
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/motors.h"
+#include "pros/motors.hpp"
+
+// Motor Ports
+const int FRONT_LEFT_MOTOR_PORT = -1;
+const int MIDDLE_LEFT_MOTOR_PORT = -2;
+const int BACK_LEFT_MOTOR_PORT = -3;
+const int FRONT_RIGHT_MOTOR_PORT = 4;
+const int MIDDLE_RIGHT_MOTOR_PORT = 5;
+const int BACK_RIGHT_MOTOR_PORT = 6;
+
+// Motor Gearing
+const pros::v5::MotorGears FRONT_LEFT_MOTOR_GEAR = pros::v5::MotorGears::green;
+const pros::v5::MotorGears MIDDLE_LEFT_MOTOR_GEAR = pros::v5::MotorGears::green;
+const pros::v5::MotorGears BACK_LEFT_MOTOR_GEAR = pros::v5::MotorGears::green;
+const pros::v5::MotorGears FRONT_RIGHT_MOTOR_GEAR = pros::v5::MotorGears::green;
+const pros::v5::MotorGears MIDDLE_RIGHT_MOTOR_GEAR = pros::v5::MotorGears::green;
+const pros::v5::MotorGears BACK_RIGHT_MOTOR_GEAR = pros::v5::MotorGears::green;
+
+// Drivetrain Settings
+const float WHEEL_DIAMETER = lemlib::Omniwheel::NEW_4;
+const float TRACK_WIDTH = 10;
+const float DRIVETRAIN_RPM = 360;
+const float HORIZONTAL_DRIFT = 2;
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+
+
+#endif // CONFIG_HPP
---
+++ src/main.cpp
@@ -0,0 +1,146 @@
+#include "main.h"
+#include "lemlib/api.hpp"
+#include "config.hpp"
+#include "main.h"
+#include "lemlib/api.hpp"
+#include "lemlib/chassis/chassis.hpp"
+#include "pros/abstract_motor.hpp"
+#include "pros/misc.h"
+
+
+// Controller
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+// Motor Ports
+pros::Motor front_left_motor(FRONT_LEFT_MOTOR_PORT, FRONT_LEFT_MOTOR_GEAR); 
+// pros::Motor middle_left_motor(MIDDLE_LEFT_MOTOR_PORT, MIDDLE_LEFT_MOTOR_GEAR); 
+pros::Motor back_left_motor(BACK_LEFT_MOTOR_PORT, BACK_LEFT_MOTOR_GEAR); 
+pros::Motor front_right_motor(FRONT_RIGHT_MOTOR_PORT, FRONT_RIGHT_MOTOR_GEAR); 
+// pros::Motor middle_right_motor(MIDDLE_RIGHT_MOTOR_PORT, MIDDLE_RIGHT_MOTOR_GEAR); 
+pros::Motor back_right_motor(BACK_RIGHT_MOTOR_PORT, BACK_RIGHT_MOTOR_GEAR); 
+// Drive Motor Group
+pros::MotorGroup left_mg({&front_left_motor, &back_left_motor});
+pros::MotorGroup right_mg({&front_right_motor, &back_right_motor});
+
+// Drivetrain Settings
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&left_mg, // left motor group
+                              &right_mg, // right motor group
+                              TRACK_WIDTH, // 10 inch track width
+                              WHEEL_DIAMETER, // using new 4" omnis
+                              DRIVETRAIN_RPM, // drivetrain rpm is 360
+                              HORIZONTAL_DRIFT// horizontal drift is 2 (for now)
+);
+
+// IMU
+pros::Imu imu_sensor(1);
+
+// Odometry
+lemlib::OdomSensors odom_sensors(nullptr,
+								nullptr,
+  					            nullptr,
+   							    nullptr,
+    							nullptr
+);
+
+// Chassis
+lemlib::Chassis chassis(
+	drivetrain, // drivetrain
+	lateral_controller, // lateral controller
+	angular_controller, // angular controller
+	odom_sensors // odom sensors
+);
+
+
+
+/**
+ * A callback function for LLEMU's center button.
+ *
+ * When this callback is fired, it will toggle line 2 of the LCD text between
+ * "I was pressed!" and nothing.
+ */
+void on_center_button() {
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
+}
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
+
+	pros::lcd::register_btn1_cb(on_center_button);
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+
+	while (true) {
+		// pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		//                  (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+		//                  (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+
+		// Print the Analog Joystick values
+		pros::lcd::print(0, "Left Joystick: %d", master.get_analog(ANALOG_LEFT_Y));
+		pros::lcd::print(1, "Right Joystick: %d", master.get_analog(ANALOG_RIGHT_X));
+
+		// Drive the robot
+		chassis.arcade(master.get_analog(ANALOG_LEFT_Y), master.get_analog(ANALOG_RIGHT_X));
+
+		pros::delay(20);                               // Run for 20 ms then update
+	}
+}
